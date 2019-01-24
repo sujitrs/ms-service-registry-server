fe4b9a1f9e9d92ae8b8379361bff2fc0e583d6e3
sujitrs <suryawanshisujeet@gmail.com>
2019-01-24 14:08:01 +0530

    init

---
+++ src/main/java/org/sj/msserviceregistryserver/MsServiceRegistryServerApplication.java
@@ -0,0 +1,14 @@
+package org.sj.msserviceregistryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServiceRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServiceRegistryServerApplication.class, args);
+	}
+
+}
+
